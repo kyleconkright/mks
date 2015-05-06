191e5cb1eeaac13bb30cc79cf2c92255f9d5842b
Mike Pacheco <mikelnpacheco@gmail.com>
2015-05-06 15:12:09 -0700

    set up active mailer

---
+++ db/migrate/20150505233338_add_columns_to_user.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :street1, :string
+    add_column :users, :street2, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :zip, :integer
+  end
+end
