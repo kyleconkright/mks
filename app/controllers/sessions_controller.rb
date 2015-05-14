df4f91225abbc60439f256f502a2d95289f6bc18
Sarah Kruberg <sarah.kruberg@gmail.com>
2015-05-14 15:21:55 -0700

    twitter reactivated. twitter_login? validation added for email and password validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
 
+def new
+end
+
 def create
 	@user = User.find_or_create_from_auth_hash(auth_hash)
-    session[:user_id] = @user.id
+    session[:user_id] = @user.id.to_s 
     redirect_to root_path
 end
+
+def destroy 
+# delete that key/value pair in our sessions hash 
+  session.delete(:user_id) 
+  redirect_to root_path
+end 
+
 
 protected
   def auth_hash
