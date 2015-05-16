b6f705c342bc61bcbdaf60fdd9c2cebd92aec350
kyleconkright <kyleconkright@gmail.com>
2015-05-16 10:39:54 -0700

    fixed double info in migration file

--- db/migrate/20150515030905_add_product_details_to_order.rb
+++ db/migrate/20150515030905_add_product_details_to_order.rb
@@ -1,7 +1,5 @@
 class AddProductDetailsToOrder < ActiveRecord::Migration
   def change
-    add_column :orders, :quanitity, :integer
-    add_column :orders, :size, :string
     add_column :orders, :status, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150514213914) do
-
+ActiveRecord::Schema.define(version: 20150515030905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +20,9 @@ ActiveRecord::Schema.define(version: 20150514213914) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.string   "status"
     t.integer  "quantity"
     t.string   "size"
+    t.string   "status"
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
